33bfa843632d6633f861cfa92a8e26af5363701e
minhthinh <phamminhthinh2003@gmail.com>
2025-04-10 02:24:16 +0700

    thêm tính năng thanh toán điện tử

---
+++ BookWeb/Services/Momo/IMomoService.cs
@@ -0,0 +1,11 @@
+﻿using BookWeb.Models.Momo;
+using BookWeb.Models;
+
+namespace BookWeb.Services.Momo
+{
+    public interface IMomoService
+    {
+        Task<MomoCreatePaymentResponseModel> CreatePaymentMomo(OrderInfo model);
+        MomoExecuteResponseModel PaymentExecuteAsync(IQueryCollection collection);
+    }
+}
---
+++ BookWeb/Services/Vnpay/IVnPayService.cs
@@ -0,0 +1,10 @@
+﻿using BookWeb.Models.Vnpay;
+
+namespace BookWeb.Services.Vnpay
+{
+    public interface IVnPayService
+    {
+        string CreatePaymentUrl(PaymentInformationModel model, HttpContext context);
+        PaymentResponseModel PaymentExecute(IQueryCollection collections);
+    }
+}
