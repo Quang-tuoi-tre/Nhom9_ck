ae8ba1608322ec0fbea04862d825c9de7959ea9c
doanduy <doanduycb02az@gmail.com>
2025-04-09 19:05:55 +0700

    them view

---
+++ BookWeb/ViewModel/HomeViewModel.cs
@@ -0,0 +1,27 @@
+﻿using BookWeb.Models;
+namespace BookWeb.ViewModel
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+
+        // Top Favorited Books
+        public IEnumerable<Book> TopBooksFavorites { get; set; }
+        public int CurrentGroupFavorites { get; set; }
+        public int TotalGroupsFavorites { get; set; }
+        public int StartIndexFavorites { get; set; }
+
+        // Top Viewed Books
+        public IEnumerable<Book> TopBooksViewed { get; set; }
+        public int CurrentGroupViewed { get; set; }
+        public int TotalGroupsViewed { get; set; }
+        public int StartIndexViewed { get; set; }
+
+        // Thêm thuộc tính sách bán chạy
+        public IEnumerable<Book> TopBooksSelling { get; set; }  
+        public int TotalGroupsSelling { get; set; }
+        public int CurrentGroupSelling { get; set; }
+        public int StartIndexSelling { get; set; }
+    }
+}
