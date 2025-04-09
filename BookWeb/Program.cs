b50ad823433717f742be344d22faaeab0d6050f7
Hồ Nhật Quang <110994116+Quang-tuoi-tre@users.noreply.github.com>
2025-04-09 20:48:23 +0700

    update program.cs

--- BookWeb/Program.cs
+++ BookWeb/Program.cs
@@ -34,7 +34,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 // ID----------------------------
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddDefaultTokenProviders().AddDefaultUI().AddEntityFrameworkStores<ApplicationDbContext>();
 
-// Google Authentication ------------------
+// Google Authentication ------------------1234567866
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = IdentityConstants.ApplicationScheme;
