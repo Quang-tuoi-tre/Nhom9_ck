abc9277dc95f1eefb37ffbbd5c3eecf0f8719712
Hồ Nhật Quang <110994116+Quang-tuoi-tre@users.noreply.github.com>
2025-04-09 21:54:21 +0700

    tao conflict

--- BookWeb/Program.cs
+++ BookWeb/Program.cs
@@ -34,7 +34,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 // ID----------------------------
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddDefaultTokenProviders().AddDefaultUI().AddEntityFrameworkStores<ApplicationDbContext>();
 
-//Quang đẹp trai quá đi à
+//Quang đẹp trai thiệt à ?
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = IdentityConstants.ApplicationScheme;
